aeb8f5e692664597d911014ffe4e8e6f12e315c2
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2022-03-17 01:27:06 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Omikuji
+
+A description of this package.
---
+++ Tests/OmikujiTests/OmikujiTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Omikuji
+
+final class OmikujiTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Omikuji().text, "Hello, World!")
+    }
+}
